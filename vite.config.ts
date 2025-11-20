6ef4874520d516b1d52699c20beff41ffaec015e
Dennis K <denniskarnowitsch@gmail.com>
2025-11-20 19:53:15 +0100

    Allow higher chunk sizes for ThreeJS

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,20 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
-  server: {
-    host: true,
-  },
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		host: true,
+	},
+	build: {
+		chunkSizeWarningLimit: 1000,
+		rollupOptions: {
+			output: {
+				manualChunks: (id) => {
+					if (id.includes("node_modules/three")) {
+						return "three";
+					}
+				},
+			},
+		},
+	},
 });
