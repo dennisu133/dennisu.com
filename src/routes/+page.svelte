b034f8888427f8287c777c90a34b96603d9d8985
Dennis K <denniskarnowitsch@gmail.com>
2026-07-01 12:46:31 +0200

    Add hyphen to "Full Stack" (very important)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 
 	const profile = {
 		name: "Dennis Karnowitsch",
-		subtitle: "Full Stack Developer",
+		subtitle: "Full-Stack Developer",
 		proficiencies: "Experienced in Python, TypeScript, Java, Go and C.",
 		availability: "Currently looking for a full-time software role and open to freelance projects.",
 		location: {
