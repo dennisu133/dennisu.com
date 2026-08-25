678ea2e2608b817809cbbf85f96605ba99e8b5b0
Dennis K <denniskarnowitsch@gmail.com>
2026-08-26 01:46:53 +0200

    chore: update packages, audit, and move svelte config inside vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import devtoolsJson from "vite-plugin-devtools-json";
+import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			preprocess: vitePreprocess(),
+			adapter: adapter({
+				precompress: true // Generate .gz and .br files for smaller transfers
+			})
+		})
+	],
 	server: { host: true },
 
 	build: {
