4ce6517afa28f5b3a8f26a62e59f340b9e15a10c
Dennis K <denniskarnowitsch@gmail.com>
2026-09-01 08:09:04 +0200

    style: increase font size in emailcontact

--- src/lib/components/EmailContact.svelte
+++ src/lib/components/EmailContact.svelte
@@ -48,7 +48,7 @@
 	<AtSign aria-hidden="true" class="size-7 shrink-0 p-1" />
 
 	<div class="min-w-0">
-		<h3 class="text-sm font-medium">
+		<h3 class="text-base font-medium">
 			<a
 				{href}
 				class="inline-flex min-h-6 w-56 items-center transition-colors duration-150 select-none group-hover/email:text-accent-hover"
