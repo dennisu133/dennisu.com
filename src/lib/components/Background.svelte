c69e9a508ccf2050f3dfff8f025f1ce6a6b9326f
Dennis K <denniskarnowitsch@gmail.com>
2026-09-07 14:28:22 +0200

    style: update background yet again

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -1,25 +1,23 @@
-<div class="background" aria-hidden="true"></div>
+<div class="background pointer-events-none fixed inset-0 -z-10" aria-hidden="true">
+	<div class="beam absolute inset-0"></div>
+	<div class="noise absolute inset-0 opacity-5"></div>
+</div>
 
 <style>
 	.background {
-		--sky-dark: oklch(10.457% 0.0125 250.212);
-		--sky-top: light-dark(oklch(82.836% 0.0927 243.863), var(--sky-dark));
-		--sky-bottom: light-dark(oklch(63.867% 0.1448 251.385), var(--sky-dark));
-		--cloud: light-dark(oklch(100% 0 none / 0.32), transparent);
+		--gradient-top: light-dark(oklch(82.836% 0.0927 243.863), oklch(15% 0.025 250));
+		--gradient-bottom: light-dark(oklch(63.867% 0.1448 251.385), oklch(4% 0.008 250));
 
-		position: fixed;
-		z-index: -1;
-		inset: 0;
-		pointer-events: none;
-		background:
-			radial-gradient(ellipse 50% 80% at 25% 75%, var(--cloud), transparent 75%),
-			radial-gradient(ellipse 70% 70% at 60% 25%, var(--cloud), transparent 75%),
-			linear-gradient(155deg, var(--sky-top), var(--sky-bottom));
+		background: linear-gradient(155deg, var(--gradient-top), var(--gradient-bottom));
 	}
 
-	@media (forced-colors: active) {
-		.background {
-			display: none;
-		}
+	.beam {
+		background: conic-gradient(
+			from calc(180deg - atan2(100vw, 100vh) - 35deg) at -10% -10%,
+			transparent,
+			light-dark(oklch(100% 0 none / 40%), transparent) 30deg 55deg,
+			transparent 100deg
+		);
+		mask-image: radial-gradient(ellipse at -10% -10%, black, transparent);
 	}
 </style>
